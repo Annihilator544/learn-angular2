f8aec11a1071ce81dba8758fb353bf263213e557
Aditya Sharma <absolutecarnage544@gmail.com>
2025-10-26 19:35:01 +0530

    11-optimizing-images

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,12 +1,15 @@
+import { NgOptimizedImage } from '@angular/common';
 import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [NgOptimizedImage],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
 export class User {
+  logoUrl = '/assets/logo.svg';
+  logoAlt = 'Angular logo';
   username = 'youngTech';
   occupation = input.required<string>();
   users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
