c0bd82ecb9c944a25b87be40c7abf8abefe713ba
Aditya Sharma <absolutecarnage544@gmail.com>
2025-10-26 18:51:13 +0530

    3-composing-components

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { User } from './user/user';
 
 @Component({
   selector: 'app-root',
   template: `
   Hello {{ city }}! Welcome to {{ title() }}, {{ 1 + 1 }}!
+  <section><app-user /></section>
 `,
+  imports: [User],
   styles: `
   :host {
     color: #a144eb;
