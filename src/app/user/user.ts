c8c76aca71c41a394988c987ac0a3084e1313a96
Aditya Sharma <absolutecarnage544@gmail.com>
2025-10-26 19:56:11 +0530

    15-forms

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,9 +1,10 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage, FormsModule],
   templateUrl: './user.html',
   styleUrl: './user.scss',
 })
@@ -11,6 +12,7 @@ export class User {
   logoUrl = '/assets/logo.svg';
   logoAlt = 'Angular logo';
   username = 'youngTech';
-  occupation = input.required<string>();
+  occupation = "hello";
+  favoriteFramework = ''
   users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
 }
