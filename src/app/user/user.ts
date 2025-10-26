106e32ebc4fb96fc7ced6c6d7971e847aefd4354
Aditya Sharma <absolutecarnage544@gmail.com>
2025-10-26 19:58:22 +0530

    16-form-control-values

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -14,5 +14,8 @@ export class User {
   username = 'youngTech';
   occupation = "hello";
   favoriteFramework = ''
+  showFramework() {
+    alert(this.favoriteFramework);
+  }
   users = [{id: 0, name: 'Sarah'}, {id: 1, name: 'Amy'}, {id: 2, name: 'Rachel'}, {id: 3, name: 'Jessica'}, {id: 4, name: 'Poornima'}]
 }
